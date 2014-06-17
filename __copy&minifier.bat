b7b3c8a426565d521e9c2e2e387af057e31ea3ac
jcolombo1 <jcolombo@ymail.com>
2014-06-17 20:37:54 -0300

    cambios menores

--- __copy&minifier.bat
+++ __copy&minifier.bat
@@ -6,11 +6,11 @@ xcopy /Y /D "..\TExpreso.templates\paginatorGrails.js"
 xcopy /Y /D "..\TExpreso.templates\tpl_tbs3.tpl" "test\files"
 
 REM ... minify los js propios ... 
-"../minify/jsmin.exe" <ClientMVC.js >ClientMVC.min.js "(c)2013 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
-"../minify/jsmin.exe" <AjaxGrails.js >AjaxGrails.min.js "(c)2013 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
-"../minify/jsmin.exe" <docCookies.js >docCookies.min.js "(c)2013 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
+"../minify/jsmin.exe" <ClientMVC.js >ClientMVC.min.js "(c)2014 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
+"../minify/jsmin.exe" <AjaxGrails.js >AjaxGrails.min.js "(c)2014 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
+"../minify/jsmin.exe" <docCookies.js >docCookies.min.js "(c)2014 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
 
 REM ... anexar los js en un solo min.js (ClientMVC.full.min.js) ...
 copy AjaxGrails.js+"..\TExpreso.js\TExpreso.js"+"..\TExpreso.js\jquery.TExpreso.js"+dataAdapter.js+paginatorGrails.js+docCookies.js+ClientMVC.js  zzz.js
-"../minify/jsmin.exe" <zzz.js >ClientMVC.full.min.js "(c)2013 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
+"../minify/jsmin.exe" <zzz.js >ClientMVC.full.min.js "(c)2014 Jorge Colombo - jcolombo@ymail.com - t: @jcolombo_ - f: /Jorge Colombo"
 del zzz.js
